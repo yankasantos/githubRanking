cb8e3a9f866f873b135d8bbad783ff355d0bdbe9
rjLelis <renatojlelis@gmail.com>
2020-05-28 18:27:46 -0300

    adicionando variavel de ambiente, e ajustando para o deploy do pront

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://api.github.com/',
-    
-})
+    baseURL: process.env.REACT_APP_API_GITHUB_URL
+});
 
 export default api;
